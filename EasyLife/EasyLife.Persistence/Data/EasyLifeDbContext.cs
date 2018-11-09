66e1e90c1fcb848003302907e3d8d59322a75435
nikolay-danev <nikolay.danev16@gmail.com>
2018-11-09 16:04:41 +0200

    Initial push

---
+++ EasyLife/EasyLife.Persistence/Data/EasyLifeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyLife.Persistence.Data
+{
+    public class EasyLifeDbContext : IdentityDbContext
+    {
+        public EasyLifeDbContext(DbContextOptions<EasyLifeDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
