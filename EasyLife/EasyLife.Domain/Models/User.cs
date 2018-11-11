294cbd1855031097f182a801c762060aef74ab5a
nikolay-danev <nikolay.danev16@gmail.com>
2018-11-11 15:49:16 +0200

    Updated to .net core 2.2

---
+++ EasyLife/EasyLife.Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyLife.Domain.Models
+{
+    public class User : IdentityUser
+    {
+        public DateTime CreatedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+    }
+}
