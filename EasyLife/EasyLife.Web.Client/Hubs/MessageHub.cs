d742b82625e50e276dfdb0af7d52f1397358cbc5
nikolay-danev <nikolay.danev16@gmail.com>
2018-12-14 16:27:52 +0200

    live chat updated

--- EasyLife/EasyLife.Web.Client/Hubs/MessageHub.cs
+++ EasyLife/EasyLife.Web.Client/Hubs/MessageHub.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EasyLife.Application.Services;
 using EasyLife.Domain.Models;
 using EasyLife.Persistence.Data;
 using Microsoft.AspNetCore.Authorization;
@@ -17,16 +19,36 @@ namespace EasyLife.Web.Client.Hubs
 		private readonly EasyLifeDbContext _context;
 		private readonly UserManager<User> _userManager;
 
+		private static readonly ConnectionMapping<string> Connections =
+			new ConnectionMapping<string>();
+
 		public MessageHub(EasyLifeDbContext context, UserManager<User> userManager)
 		{
 			_context = context;
 			_userManager = userManager;
 		}
 
+		public override Task OnConnectedAsync()
+		{
+			string name = Context.User.Identity.Name;
+
+			Connections.Add(name, Context.ConnectionId);
+
+			return base.OnConnectedAsync();
+		}
+
+		public override Task OnDisconnectedAsync(Exception exception)
+		{
+			string name = Context.User.Identity.Name;
+
+			Connections.Remove(name, Context.ConnectionId);
+			return base.OnDisconnectedAsync(exception);
+		}
+
 		public async Task SendMessage(string senderEmail, string receiverEmail, string messageContent)
 		{
 			var sender = await this._userManager.Users.FirstOrDefaultAsync(x => x.Email == senderEmail);
-			var receiver = await this._userManager.Users.FirstOrDefaultAsync(x => x.Email == receiverEmail);
+			var receiver = await this._userManager.Users.FirstOrDefaultAsync(x => x.UserName == receiverEmail);
 			var newMessage = new Message
 			{
 				Content = messageContent,
@@ -36,20 +58,31 @@ namespace EasyLife.Web.Client.Hubs
 				SenderId = sender.Id,
 			};
 
-			await this._context.Messages.AddAsync(newMessage);
+			//await this._context.Messages.AddAsync(newMessage);
+			//await this._context.SaveChangesAsync();
+			//Connections.TryGetValue(receiverEmail, out var connectionToSendMessage);
 
-			await this._context.SaveChangesAsync();
+			//if (!string.IsNullOrWhiteSpace(connectionToSendMessage))
+			//{
+			//await Clients.Caller.SendAsync("ReceiveMessage", newMessage);
+			//await Clients.Group(receiver.UserName).SendAsync("ReceiveMessage", newMessage);
+			//}
 
-			await this.Clients.Caller.SendAsync("ReceiveMessage", newMessage.Content);
+			foreach (var connectionId in Connections.GetConnections(receiver.UserName))
+			{
+				await Clients.Caller.SendAsync("ReceiveMessage", newMessage);
+				await Clients.Client(connectionId).SendAsync("ReceiveMessage",  newMessage);
+			}
 		}
 
-		public async Task GetMessages()
+
+	public async Task GetMessages()
 		{
 			var caller = await this._userManager.Users.FirstOrDefaultAsync(x => x.Email == this.Context.User.Identity.Name);
 
-			var messages = await this._context.Messages.Where(x => x.SenderId == caller.Id).FirstOrDefaultAsync();
+			var messages = await this._context.Messages.Where(x => x.SenderId == caller.Id).ToArrayAsync();
 
-			await this.Clients.Caller.SendAsync("ReceiveAllMessages", messages.Content);
+			await this.Clients.Caller.SendAsync("ReceiveAllMessages", messages);
 		}
 	}
 }
--- EasyLife/EasyLife.Web.Client/wwwroot/js/message.js
+++ EasyLife/EasyLife.Web.Client/wwwroot/js/message.js
@@ -2,26 +2,35 @@
 
 connection.on("ReceiveMessage",
     function (message) {
-        var chatInfo = `<div>${message}</div>`;
+        let chatInfo = "<div class=\"chat-message clearfix\" ><div class=\"chat-message-content clearfix\"><p>" + message.content + "</p></div></div ><hr>";
         $("#messagesList").append(chatInfo);
     });
 
-connection.on("ReceiveAllMessages", function (messages)
-{
-    var chatInfo = `<div>${messages}</div>`;
-    $("#messagesList").append(chatInfo);
+$("#newMessage").keyup(function (e) {
+    if (e.keyCode === 13) {  //checks whether the pressed key is "Enter"
+        //$("#sendNewMessage").click(function () {
+        let senderEmail = $("#senderEmail").val();
+        let receiverEmail = $("#receiverEmail").val();
+        let message = $("#newMessage").val();
+        if (message !== "") {
+            connection.invoke("SendMessage", senderEmail, receiverEmail, message);
+            $("#newMessage").val('');
+        }
+        //});
+    }
 });
 
-$("#getMessages").click(function() {
+connection.on("ReceiveAllMessages", function (messages) {
+    //var chatInfo = `<div>${messages}</div>`;
+    //$("#messagesList").append(chatInfo);
+    console.log(messages);
+});
+
+$("#getMessages").click(function () {
     connection.invoke("GetMessages");
 });
 
-$("#sendNewMessage").click(function () {
-    var senderEmail = $("#senderEmail").val();
-    var receiverEmail = $("#receiverEmail").val();
-    var message = $("#newMessage").val();
-    connection.invoke("SendMessage", senderEmail, receiverEmail, message);
-});
+
 
 connection.start().catch(function (err) {
     return console.error(err.toString());
